cde0e3e9cbfcd0fbb2b8703c5b95d57a1710cf64
LudoLogical <dcd180001@utdallas.edu>
2022-08-15 15:10:30 -0500

    Style fixes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,16 +3,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-return (
-  <Html>
-    <Head>
-      <link rel="icon" href="/favicon.ico" />
-      <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Muli&display=swap' />
-    </Head>
-    <body>
-      <Main />
-      <NextScript />
-    </body>
-  </Html>
-)
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Muli&display=swap' />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
